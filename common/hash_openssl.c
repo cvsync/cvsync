adeed979a91839911135acc22198edc7d8adcbd6
MAEKAWA Masahide <maekawa@daemon-systems.org>
2026-03-15 03:02:03 +0900

    Unify update process. Improve final process.

--- common/hash_openssl.c
+++ common/hash_openssl.c
@@ -15,30 +15,29 @@
 #include "hash.h"
 
 bool cvsync_MD5_init(void **);
-void cvsync_MD5_update(void *, const void *, size_t);
 void cvsync_MD5_final(void *, uint8_t *);
 bool cvsync_SHA1_init(void **);
-void cvsync_SHA1_update(void *, const void *, size_t);
 void cvsync_SHA1_final(void *, uint8_t *);
 bool cvsync_SHA256_init(void **);
-void cvsync_SHA256_update(void *, const void *, size_t);
 void cvsync_SHA256_final(void *, uint8_t *);
+void cvsync_openssl_update(void *, const void *, size_t);
+void cvsync_openssl_final(void *, uint8_t *, unsigned int *);
 void cvsync_openssl_destroy(void *);
 
 const struct hash_args MD5_args = {
-	cvsync_MD5_init, cvsync_MD5_update, cvsync_MD5_final,
+	cvsync_MD5_init, cvsync_openssl_update, cvsync_MD5_final,
 	cvsync_openssl_destroy,
 	HASH_SIZE_MD5
 };
 
 const struct hash_args SHA1_args = {
-	cvsync_SHA1_init, cvsync_SHA1_update, cvsync_SHA1_final,
+	cvsync_SHA1_init, cvsync_openssl_update, cvsync_SHA1_final,
 	cvsync_openssl_destroy,
 	HASH_SIZE_SHA1
 };
 
 const struct hash_args SHA256_args = {
-	cvsync_SHA256_init, cvsync_SHA256_update, cvsync_SHA256_final,
+	cvsync_SHA256_init, cvsync_openssl_update, cvsync_SHA256_final,
 	cvsync_openssl_destroy,
 	HASH_SIZE_SHA256
 };
@@ -58,18 +57,11 @@ cvsync_MD5_init(void **ctx)
 }
 
 void
-cvsync_MD5_update(void *ctx, const void *buffer, size_t bufsize)
-{
-	EVP_DigestUpdate(ctx, buffer, bufsize);
-}
-
-void
 cvsync_MD5_final(void *ctx, uint8_t *buffer)
 {
 	unsigned int s = HASH_SIZE_MD5;
 
-	EVP_DigestFinal_ex(ctx, buffer, &s);
-	EVP_MD_CTX_free(ctx);
+	cvsync_openssl_final(ctx, buffer, &s);
 }
 
 bool
@@ -87,18 +79,11 @@ cvsync_SHA1_init(void **ctx)
 }
 
 void
-cvsync_SHA1_update(void *ctx, const void *buffer, size_t bufsize)
-{
-	EVP_DigestUpdate(ctx, buffer, bufsize);
-}
-
-void
 cvsync_SHA1_final(void *ctx, uint8_t *buffer)
 {
 	unsigned int s = HASH_SIZE_SHA1;
 
-	EVP_DigestFinal_ex(ctx, buffer, &s);
-	EVP_MD_CTX_free(ctx);
+	cvsync_openssl_final(ctx, buffer, &s);
 }
 
 bool
@@ -116,17 +101,23 @@ cvsync_SHA256_init(void **ctx)
 }
 
 void
-cvsync_SHA256_update(void *ctx, const void *buffer, size_t bufsize)
+cvsync_SHA256_final(void *ctx, uint8_t *buffer)
+{
+	unsigned int s = HASH_SIZE_SHA256;
+
+	cvsync_openssl_final(ctx, buffer, &s);
+}
+
+void
+cvsync_openssl_update(void *ctx, const void *buffer, size_t bufsize)
 {
 	EVP_DigestUpdate(ctx, buffer, bufsize);
 }
 
 void
-cvsync_SHA256_final(void *ctx, uint8_t *buffer)
+cvsync_openssl_final(void *ctx, uint8_t *buffer, unsigned int *s)
 {
-	unsigned int s = HASH_SIZE_SHA256;
-
-	EVP_DigestFinal_ex(ctx, buffer, &s);
+	EVP_DigestFinal_ex(ctx, buffer, s);
 	EVP_MD_CTX_free(ctx);
 }
 
